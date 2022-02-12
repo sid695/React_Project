1b1acee9545828c8f6e26c3e2f1fcdf9793c5b51
Sudhanshu <sudhanshu.chand69@gmail.com>
2022-02-12 13:30:24 +0530

    New Commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
           Log In / Registration
         </a>
         <div />
-        <a href="/" className="p__opensans">
+        <a href="#" className="p__opensans">
           Book Table
         </a>
       </div>
